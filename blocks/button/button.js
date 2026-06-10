4eeaacea37d5211ce95e2792517ccb3fb567d552
JuliusRomero <jromero@crescendocollective.com>
2026-06-10 13:32:43 +0800

    test

---
+++ blocks/button/button.js
@@ -0,0 +1,24 @@
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+  const [linkRow, textRow, titleRow, typeRow] = [...block.children].map(
+    (row) => row.firstElementChild,
+  );
+
+  const a = linkRow?.querySelector('a');
+  if (!a) return;
+
+  const linkText = textRow?.textContent.trim();
+  const linkTitle = titleRow?.textContent.trim();
+  const linkType = typeRow?.textContent.trim();
+
+  if (linkText) a.textContent = linkText;
+  if (linkTitle) a.title = linkTitle;
+  a.className = ['button', linkType].filter(Boolean).join(' ');
+
+  const p = document.createElement('p');
+  p.className = 'button-wrapper';
+  moveInstrumentation(block, p);
+  p.append(a);
+  block.replaceWith(p);
+}
